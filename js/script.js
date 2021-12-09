1dff1769e0a1f7c241a47b21cf2f79a7191156df
Claudio <claudiodicaprio89@gmail.com>
2021-12-09 15:26:15 +0100

    function

--- js/script.js
+++ js/script.js
@@ -2,5 +2,14 @@ const app = new Vue({
     el: '#root',
     data: {
       emailList : [],
+    },
+    mounted(){
+        const self = this;
+        for(let i = 0; i < 10; i++){
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(element){
+                self.emailList.push(element.data.response)
+            });
+        }
     }
   });
